238c5bbb9946024ad52d81ccbfe88d61287a0c1c
Matteo Imbimbo <matteo.imbimbo@yahoo.it>
2022-05-13 18:33:35 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        // DICHIARO LE PROPRIETA
+
+        private int codice;
+        public int Codice 
+        { 
+            get 
+            {
+                return codice;
+            }
+            
+        }
+        private string nome;
+        public string Nome 
+        {
+            get
+            { 
+                return nome;
+            }
+
+            set
+            {
+                if (value.Length <= 0)
+                    Console.WriteLine("inserisci un nome valido");
+                else
+                    nome = value;
+            }
+        }
+        private string descrizione;
+        public string Descrizione {
+            get
+            {
+                return descrizione;
+            }
+            set
+            {
+                if (value.Length < 10)
+                    throw new Exception("Inserisci una descrizione piu lunga");
+                else
+                    descrizione = value;    
+            }
+        }
+        private double prezzo;
+        public double Prezzo {
+            get
+            { 
+                return prezzo;
+            }
+            set 
+            {
+                if (double.IsNaN(value) || value <= 0)
+                   throw new Exception("Il prezzo non e valido");
+                else
+                    prezzo = value;
+            }
+        }
+        private double iva;
+        public double Iva
+        {
+            get { return iva; }
+            set { iva = value; }
+        }
+
+        private static Random random = new Random();
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            //random = new Random();
+            codice = random.Next();
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+            
+        }
+
+
+        public double prezzoIvato()
+        {
+            double newPrezzo;
+            double pIva = (this.Prezzo * this.Iva)/100;
+            return newPrezzo = this.Prezzo + pIva;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Esercizio di oggi: C# Shop nome repo: csharp-oop-shop Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop. 
+ * Un prodotto è caratterizzato da: 
+ * - codice (numero intero) 
+ * - nome 
+ * - descrizione 
+ * - prezzo 
+ * - iva 
+ * 
+ * Usate opportunamente i livelli di accesso (public, private), 
+ * i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che: 
+ * alla creazione di un nuovo prodotto il codice sia valorizzato 
+ * con un numero random 
+ * Il codice prodotto sia accessibile solo in lettura 
+ * Gli altri attributi siano accessibili sia in lettura che in scrittura 
+ * Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva 
+ * Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto. 
+ * 
+ * BONUS: create un metodo che restituisca il codice con un pad 
+ * left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+ */
+
+
+
+using csharp_oop_shop;
+using System;
+
+namespace csharp_es03_mattina // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine(random.Next(0, 10));
+
+
+            try
+            {
+
+                Prodotto prod1 = new Prodotto("pasta barilla", "pasta da fare al sugo", 1.69, 22);
+
+
+                Console.WriteLine("Codice: {0}", prod1.Codice);
+                Console.WriteLine("Nome: {0}", prod1.Nome);
+                Console.WriteLine("Descrizione: {0}", prod1.Descrizione);
+                Console.WriteLine(prod1.prezzoIvato());
+
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.Message);
+            }
+            
+             
+        }
+    }
+}
